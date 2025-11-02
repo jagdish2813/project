64d506a190f34b94876d21ee2586053dea800393
jagdish2813 <55352422+jagdish2813@users.noreply.github.com>
2025-11-02 16:51:19 +0530

    Updated AdminSignup.tsx

--- src/pages/AdminSignup.tsx
+++ src/pages/AdminSignup.tsx
@@ -7,10 +7,10 @@ export default function AdminSignup() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState<{ type: 'success' | 'error'; text: string } | null>(null);
-  //const [formData, setFormData] = useState({
-   // email: 'admin@gmail.com',
-   // password: 'Welcome@123'
-  //});
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
 
   const handleSignup = async (e: React.FormEvent) => {
     e.preventDefault();
